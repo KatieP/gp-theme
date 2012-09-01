626f735afaae8024d22b511cc53910fa20abbb1c
Katie Patrick <katie.patrick@thegreenpages.com.au>
2012-09-01 23:07:30 +1000

    A function that shows a google map with a marker for the location where the post is tagged. Will add radius of nearby posts to this soon.

--- gp-au-theme/loop.php
+++ gp-au-theme/loop.php
@@ -474,6 +474,7 @@ function default_single() {
 			theme_singlepagination();
 			theme_single_contributor_donate_join_bar();
 			theme_single_product_button();
+			theme_single_map();
 			theme_singlecomments();
 		echo '</article>';
 	}
@@ -1378,7 +1379,7 @@ function theme_contributorsblurb($profile_author) {
 		echo '<p>' . nl2br($profile_author->contributors_blurb) . '</p>';
 	}
 }
-
+/********************************************************************************/
 /** PRODUCT 'BUY IT!' BUTTON **/
 
 function theme_single_product_button() {
@@ -1403,6 +1404,94 @@ function theme_single_product_button() {
 	 	}
 	}
 }
+
+/** GOOGLE MAP FOR POSTS **/
+// This function grabs meta data for lat and long from each posts and displays them in a google map.
+
+function theme_single_map() {
+    if (get_post_type() != "page") { 
+        global $post;
+        
+        # Find ID 
+        $post_id = $post->ID;
+                
+        # Set location meta keys depending on post type
+        switch (get_post_type()) {
+		    case 'gp_news':
+                $lat_key = 'gp_news_google_geo_latitude';
+                $long_key = 'gp_news_google_geo_longitude';
+		        break;
+		    case 'gp_projects':
+		    	$lat_key = 'gp_projects_google_geo_latitude';
+                $long_key = 'gp_projects_google_geo_longitude';
+		        break;
+			case 'gp_advertorial':
+				$lat_key = 'gp_advertorial_google_geo_latitude';
+                $long_key = 'gp_advertorial_google_geo_longitude';
+		        break;
+			case 'gp_competitions':
+				$lat_key = 'gp_competitions_google_geo_latitude';
+                $long_key = 'gp_competitions_google_geo_longitude';
+		        break;
+		    case 'gp_events':
+		    	$lat_key = 'gp_events_google_geo_latitude';
+                $long_key = 'gp_events_google_geo_longitude';
+		        break;
+		}
+                
+        # get post location meta (post id, key, true/false)
+        $lat = get_post_meta($post_id, $lat_key, true);
+        $long = get_post_meta($post_id, $long_key, true);
+        
+        # display google map if proper location data found
+        if (!empty($lat) && !empty($long)) {
+            ?>        
+            <script type="text/javascript"
+                    src="http://maps.googleapis.com/maps/api/js?key=AIzaSyC1Lcch07tMW7iauorGtTY3BPe-csJhvCg&sensor=false">
+            </script>
+            <script type="text/javascript">
+      
+                var lat = <?php echo $lat; ?>;
+                var long = <?php echo $long; ?>;
+        
+                function initialize() {
+                    var myLatlng = new google.maps.LatLng(lat,long);
+                    var mapOptions = {
+                        zoom: 10,
+                        center: myLatlng,          
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                    }
+        
+                    var map = new google.maps.Map(document.getElementById("post_google_map_canvas"),
+                          mapOptions);
+        
+                    var marker = new google.maps.Marker({
+        	            position: myLatlng,
+      		            map: map,
+      		            title:"<?php echo $post->title; ?>"
+                    })  
+                }
+      
+                function loadScript() {
+  		            var script = document.createElement("script");
+  		            script.type = "text/javascript";
+  		            script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyC1Lcch07tMW7iauorGtTY3BPe-csJhvCg&sensor=false&callback=initialize";
+  		            document.body.appendChild(script);
+                }
+
+		        window.onload = loadScript;
+            </script>
+       
+            <div onload="initialize()">
+ 
+            <div id="post_google_map_canvas"></div>
+
+	    <?php 
+	    }
+	}
+}
+
+
 
 /********************************************************************************/
 
