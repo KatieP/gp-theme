6ddfce3727383e2064a8787c17a371234dcdeb77
Jesse Browne <jb@greenpag.es>
2013-06-19 04:39:36 +1000

    Added awesome banner by Katie for About page

--- gp-au-theme/index.php
+++ gp-au-theme/index.php
@@ -1,23 +1,16 @@
 <?php get_header(); ?>
-
 	<div class="pos">
-		<?php get_sidebar('left'); ?>
-
-			<div id="col2" class="set3col">
-				<div id="content">
-					
-					<?php
-                        /* Run the loop to output the posts.
-                         * If you want to overload this in a child theme then include a file
-                         * called loop-index.php and that will be used instead.
-                         */
-                         get_template_part( 'loop', 'index' );
-					?>
-					
-				</div>
+		<div id="col2" class="set3col">
+			<div id="content">					
+				<?php
+                /* Run the loop to output the posts.
+                 * If you want to overload this in a child theme then include a file
+                 * called loop-index.php and that will be used instead.
+                 */
+                get_template_part( 'loop', 'index' );
+				?>
 			</div>
-
+		</div>
 		<?php get_sidebar('right'); ?>
 	</div>
-
 <?php get_footer(); ?>
