3a4448cb5c5f7f128468876a37fd787ed767322e
Katie Patrick <katie.patrick@thegreenpages.com.au>
2013-06-19 09:32:25 +1000

    created tag.php + added some images

---
+++ gp-au-theme/tag.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+	<div class="pos">
+		<div id="col2" class="set3col">
+			<div id="content">					
+			
+               
+                 
+                 <p>Tag: <?php single_tag_title(); ?></p>
+
+				<p>KATIE'S TEST TAG PAGE</p>
+                 
+			</div>
+		</div>
+		<?php get_sidebar('right'); ?>
+	</div>
+<?php get_footer(); ?>
+
+
+
+
