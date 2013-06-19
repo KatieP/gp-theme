1f4b57c7ea7ed83b5d77a57eccc2b4a6d0b1af7a
Jesse Browne <jb@greenpag.es>
2013-06-20 04:30:43 +1000

    Tag page now works for news posts.

--- gp-au-theme/tag.php
+++ gp-au-theme/tag.php
@@ -2,13 +2,20 @@
 	<div class="pos">
 		<div id="col2" class="set3col">
 			<div id="content">					
-			
-               
-                 
-                 <p>Tag: <?php single_tag_title(); ?></p>
-
-				<p>KATIE'S TEST TAG PAGE</p>
-                 
+			    <?php $tag = single_tag_title("", false); ?>
+                <p><strong>Topic: <?php echo $tag; ?></strong></p>
+				
+				<?php
+				/** Display posts with relevant tag **/
+				$tag_args =   array ( 'posts_per_page' => 20,
+				                      'post_type' => 'gp_news',
+				                      'tag' => $tag );
+				$pageposts =  get_posts( $tag_args );
+				
+				foreach ($pageposts as $post) {
+				    theme_index_feed_item();
+				}
+				?>		
 			</div>
 		</div>
 		<?php get_sidebar('right'); ?>
