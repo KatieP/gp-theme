b99b827b819f96acdf6cff684cfd50ab2d33b15e
Jesse Browne <jb@greenpag.es>
2013-08-22 04:17:08 +1000

    Added get_clicks_for_posts(), proficle page analytics function is ripe refactoring

--- gp-au-theme/functions.php
+++ gp-au-theme/functions.php
@@ -4293,7 +4293,9 @@ function theme_update_delete_post() {
 			confirm_str += '</form>';
 				
 			$(function() {
-				$( '#delete-post-dialog' ).dialog({ modal: true }).html(confirm_str).delay(20000).hide(function() { $( '#delete-post-dialog' ).dialog('close') }); 
+				$( '#delete-post-dialog' ).dialog({ modal: true }).html(confirm_str).delay(20000).hide(function() { 
+					$( '#delete-post-dialog' ).dialog('close'); 
+				}); 
 			});
 		}
 
@@ -5000,6 +5002,10 @@ function get_component_id($product_id) {
 }
 
 function get_cost_per_click($product_id) {
+    /**
+     * As it sounds, returns cost per click depending 
+     * on which chargify subscription user is on
+     */
     switch ($product_id)   {
         case '3313295':
             // $12 per week plan
@@ -5029,18 +5035,105 @@ function get_cost_per_click($product_id) {
     return $cpc;   
 }
 
-/* FUNCTION TO REGISTER CUSTOM POST STATUS FOR ADVERTISERS TO TURN OFF/TURN ON POSTS */
+function get_click_cap($product_id) {
+    /**
+     * Calculate maximum clicks available per week
+     * based on which plan advertiser is signed up to.
+     * 
+     * Called by metered-billing-cron.php
+     * 
+     * Author: Jesse Browne
+     *         jb@greenpag.es
+     *  
+     **/
+    
+    switch ($product_id)   {
+        case '3313295':
+            // $12 per week plan
+            $cap = (int) (12.00 / 1.9);
+            break;
+        case '27029':
+            // $39 per week plan
+            $cap = (int) (39.00 / 1.9);
+            break;
+        case '27028':
+            // $99 per week plan
+            $cap = (int) (99.00 / 1.9);
+            break; 
+        case '3313296':
+            // $249 per week plan
+            $cap = (int) (249.00 / 1.8);
+            break; 
+        case '3313297':
+            // $499 per week plan
+            $cap = (int) (449.00 / 1.7);
+            break;
+        case '3325582':
+            // Free cpc plan
+            $cap = (int) 1000;
+            break;
+        case '27023':
+            // $39 per month old plan
+            $cap = (int) 1000;
+            break;                                              
+    }
+    
+    return $cap;
+}
 
-#function my_custom_post_status(){
-#	register_post_status( 'unread', array(
-#		'label'                     => _x( 'post_hidden'),
-#		'public'                    => true,
-#		'exclude_from_search'       => false,
-#		'show_in_admin_all_list'    => true,
-#		'show_in_admin_status_list' => true,
-#		'label_count'               => _n_noop( 'Unread <span class="count">(%s)</span>', 'Unread <span class="count">(%s)</span>' ),
-#	) );
-#}
-#add_action( 'init', 'my_custom_post_status' );
+function get_clicks_for_post($post_row, $user_id, $analytics, $start_range, $end_range) {
+    /**
+     * Returns total outbound clicks from a post from Google Analytics,
+     * gets product button clicks also. 
+     * 
+     * Called by metered-billing-cron.php and weekly-advertiser-email.php
+     * 
+     * @TODO Refactor the shit out of the analytics function for profile page, 
+     *       this function could be called from there. 
+     *       Maybe move analytics functions to separate file?
+     *       
+     * Authors: Initial work on analytics done by 
+     *          Katie Patrick, Stephanie 'Cord' Melton &
+     *          Jesse Browne
+     *          jb@greenpag.es
+     * 
+     */
+    
+	$analytics->setDateRange($start_range, $end_range);	        //Set date in GA $analytics->setMonth(date('$post_date'), date('$new_date'));
+
+   	#SET UP POST ID AND AUTHOR ID DATA, POST DATE, GET LINK CLICKS DATA FROM GA 
+	$profile_author_id =  $user_id;
+	$post_id =            $post_row->ID;
+	$click_track_tag =    '/yoast-ga/' . $post_id . '/' . $profile_author_id . '/outbound-article/';
+
+	$clickURL = ($analytics->getPageviewsURL($click_track_tag));
+	$sumClick = 0;
+
+	foreach ($clickURL as $data) {
+   		$sumClick = $sumClick + $data;
+	}
+
+    // Get url product button is linked to
+    $sql_product_url = 'SELECT meta_value 
+                        FROM wp_postmeta 
+                        WHERE post_id = "'. $post_id .'"
+                            AND meta_key = "gp_advertorial_product_url";';
+
+    $product_url_results =  mysql_query($sql_product_url);
+    mysql_data_seek($product_url_results, 0);
+    $product_url_row =      mysql_fetch_object($product_url_results);	
+	$product_url =          $product_url_row->meta_value;
+
+	if ( !empty($product_url) ) {		# IF 'BUY IT' BUTTON ACTIVATED, GET CLICKS
+	    $click_track_tag_product_button = '/outbound/product-button/' . $post_id . '/' . $profile_author_id . '/' . $product_url . '/'; 	         
+		$clickURL_product_button = ($analytics->getPageviewsURL($click_track_tag_product_button));
+            
+		foreach ($clickURL_product_button as $data) {
+   			$sumClick = $sumClick + $data;
+		}
+	}
+        
+    return $sumClick;
+}
 
 ?>
